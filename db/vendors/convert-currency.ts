64f379bb3f76368b9a7e9cc8ff06e6e0bb1bb07a
Ryddag <27833747+Ryddag@users.noreply.github.com>
2019-09-15 05:19:33 +0200

    Update convert-currency.ts

--- db/vendors/convert-currency.ts
+++ db/vendors/convert-currency.ts
@@ -10,11 +10,17 @@ export function convertCurrency(id: string): Vendor["currency"] {
   if (id.includes("cCapResFactionPoints4")) {
     return "sparta"
   }
+  if (id.includes("cCapResFactionPoints5")) {
+    return "crete"
+  }
   if (id.includes("cCapResFactionPoints1")) {
     return "halloween"
   }
   if (id.includes("cCapResFactionPoints2")) {
     return "winter"
+  }
+  if (id.includes("cCapResFactionPoints3")) {
+    return "summer"
   }
   throw new Error("Unknown currency: " + id)
 }
