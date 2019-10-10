d29ba2c6dd9c62501b5687984fae6b46dee979f0
SystemGlitch <jeremy.la@outlook.fr>
2019-10-10 20:02:27 +0200

    Generate consumables sprite

--- db/consumables/convert.ts
+++ db/consumables/convert.ts
@@ -7,6 +7,11 @@ import { buildSearchString } from "./search"
 export async function convertConsumable(consumable: any): Promise<Consumable> {
   const name = await translateEn(consumable.displaynameid, consumable.name)
   const description = await translateEn(consumable.rollovertextid, "")
+
+  // Mummy exception
+  if(consumable.icon == 'Celeste\\HalloweenEvent\\Mummy\\ConMummyFighter_ua') {
+    consumable.icon = 'Celeste\\HalloweenEvent\\Mummy\\Mummy64_ua'
+  }
   const icon = await downloadIcon(`Art/${consumable.icon}`, "consumables")
   const rarity = consumable.rarity.replace("cRarity", "").toLowerCase()
 
