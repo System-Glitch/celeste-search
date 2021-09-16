75cb3176ade055d3c65e0379bd6178c89370abfc
Andrei Bratu <andrei.bratu@gmail.com>
2021-09-16 11:55:30 +0200

    Mark all Crete and Skirmish Items as such
    Mark all Crete and Skirmish Items as such

--- db/items/convert-loot-table.ts
+++ db/items/convert-loot-table.ts
@@ -1,7 +1,7 @@
 import { Trait } from "../../api-types"
 
 export function convertLootTable(trait: Trait): string | undefined {
-  if (trait.dbid <= 2258 && trait.name.includes('_')) {
+  if ((trait.dbid <= 2258 || trait.dbid === 2419) && trait.name.includes('_')) {
     var splitName = trait.name.split('_');
     switch (splitName[1].charAt(1)) {
       case "a":
@@ -15,5 +15,11 @@ export function convertLootTable(trait: Trait): string | undefined {
   }
   if (trait.name.endsWith("_moes")) {
     return "moe's mystorium"
+  }
+  if (trait.name.startsWith("gear_bldg_ra") || trait.name.startsWith("gear_bldg_ua")) {
+    return "crete"
+  }
+  if (trait.name.startsWith("gear_bldg_rb") || trait.name.startsWith("gear_bldg_ub") || trait.name.startsWith("gear_boat_rb") || trait.name.startsWith("gear_prst_rb") || trait.name.startsWith("gear_siege_eb")) {
+    return "skirmish hall"
   }
 }
