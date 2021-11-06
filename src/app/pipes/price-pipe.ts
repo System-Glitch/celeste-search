2f1336a57498a2c101201b8c43aa5744c4e58b68
Andrei Bratu <andrei.bratu@gmail.com>
2021-11-06 19:30:38 +0100

    updated data
    updated data

--- src/app/pipes/price-pipe.ts
+++ src/app/pipes/price-pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core"
 export class PricePipe implements PipeTransform {
 
   transform(price: number): string {
+    /*
     if (price >= 1000 * 1000 * 10) {
       return (price / 1000 / 1000).toFixed(0) + "M"
     }
@@ -16,10 +17,14 @@ export class PricePipe implements PipeTransform {
       return (price / 1000).toFixed(0) + "k"
     }
     if (price >= 1000) {
-      return (price / 1000).toFixed(1).replace(/.0$/, "") + "k"
-    }
-    if (price >= 1000) {
       return (price / 1000).toFixed(0) + "k"
+    }
+    */
+    if (price >= 1000 && price % 1000 === 0) {
+      return (price / 1000).toFixed(3).replace("\.000", "") + "k"
+    }
+    if (price >= 1000 && price % 1000 !== 0 && price % 100 === 0) {
+      return (price / 1000).toFixed(1).replace("/.0$/", "") + "k"
     }
     return price.toFixed(0)
   }
